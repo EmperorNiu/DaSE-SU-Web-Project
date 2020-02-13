bfffd9a2da340a8c6c1d73e8acbe53d15ec0ba1c
EmperorNiu <niuyuean99@sina.com>
2020-02-14 02:42:36 +0800

    update the user page

--- fe/src/components/Frame.vue
+++ fe/src/components/Frame.vue
@@ -30,7 +30,44 @@
       <!-- </div> -->
     </el-header>
     <el-container>
-      <el-aside width="150px">Aside</el-aside>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+
+            <!-- 二级菜单 -->
+            <!-- <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>-->
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>
         <router-view></router-view>
       </el-main>
@@ -40,7 +77,34 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      menulist: [
+        { id: '0', authName: '第一目录' },
+        { id: '1', authName: '第二目录' },
+        { id: '2', authName: '第三目录' }
+      ],
+      // 是否折叠
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: 'xxx',
+      iconsObj: {
+        0: 'iconfont icon-user',
+        1: 'iconfont icon-tijikongjian',
+        2: 'iconfont icon-shangpin',
+        3: 'iconfont icon-danju',
+        4: 'iconfont icon-baobiao'
+      }
+    }
+  },
+  methods: {
+    // 点击按钮，切换菜单的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -72,8 +136,27 @@ export default {}
 .home-container {
   height: 100%;
 }
-.el-menu {
+// .el-menu {
+//   // margin-left: 75px;
+//   border-bottom: 0 !important;
+// }
+.home-menu {
   margin-left: 75px;
   border-bottom: 0 !important;
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
--- fe/src/components/User.vue
+++ fe/src/components/User.vue
@@ -8,15 +8,24 @@
       <el-breadcrumb-item>活动列表</el-breadcrumb-item>
       <el-breadcrumb-item>活动详情</el-breadcrumb-item>
     </el-breadcrumb>
-    <div class="block">
-      <el-avatar :size="85" :src="circleUrl"></el-avatar>
+    <div class="aLine"></div>
+    <div class="intro">
+      <div>
+        <el-avatar :size="85" :src="circleUrl"></el-avatar>
+      </div>
+      <div class="text-intro">
+        <div class="text-pattern1">{{username}}</div>
+        <div class="text-pattern1">{{major}}</div>
+      </div>
     </div>
+
     <div class="tags">
-      <el-tag>标签一</el-tag>
-      <el-tag type="success">标签二</el-tag>
-      <el-tag type="info">标签三</el-tag>
-      <el-tag type="warning">标签四</el-tag>
-      <el-tag type="danger">标签五</el-tag>
+      <div>标签：</div>
+      <el-tag>前端工程师</el-tag>
+      <el-tag type="success">摄影师</el-tag>
+      <el-tag type="info">PS达人</el-tag>
+      <el-tag type="warning">Python</el-tag>
+      <el-tag type="danger">javascript</el-tag>
     </div>
   </div>
 </template>
@@ -26,8 +35,42 @@ export default {
   data() {
     return {
       circleUrl:
-        'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+        'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+      username: '开发者X7',
+      major: '数据科学与工程学院'
     }
   }
 }
 </script>
+
+<style lang="less" scoped>
+.aLine {
+  background-color: rgb(146, 146, 146);
+  height: 1px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+.intro {
+  display: flex;
+  flex-direction: row;
+  margin-left: 10px;
+}
+.text-intro {
+  margin-left: 50px;
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+}
+.text-pattern1 {
+  font-size: 18px;
+  margin-bottom: 10px;
+  word-spacing: 15px;
+}
+.tags {
+    margin-top: 20px;
+    margin-left: 20px;
+}
+.el-tag {
+    margin: 5px;
+}
+</style>
